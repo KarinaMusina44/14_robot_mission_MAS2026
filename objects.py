18031638dcc52226a0162e9d3dd600f908e9f87f
DeodatoBastos <deodatobasto@gmail.com>
2026-03-16 14:41:53 +0100

    add objects

---
+++ objects.py
@@ -0,0 +1,30 @@
+import random
+
+from mesa import Agent
+
+
+class Radioactivity(Agent):
+    def __init__(self, model, zone):
+        super().__init__(model)
+        self.zone = zone
+
+        if zone == 'z3':
+            self.radioactivity = random.uniform(0.66, 1.0)
+        elif zone == 'z2':
+            self.radioactivity = random.uniform(0.33, 0.66)
+        elif zone == 'z1':
+            self.radioactivity = random.uniform(0.0, 0.33)
+        else:
+            raise ValueError("Invalid zone. Choose 'z1', 'z2', or 'z3'.")
+
+
+class WasteDisposalZone(Agent):
+    def __init__(self, model, zone):
+        super().__init__(model)
+        self.zone = zone
+
+
+class Waste(Agent):
+    def __init__(self, model, waste_type):
+        super().__init__(model)
+        self.waste_type = waste_type
